e366ddad4de3f1e6c31b2c1fe21a10badc0c6ed9
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-11-02 23:19:07 -0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+fn title() {
+    println!("  ---     ---     ---     ");
+    println!("  Want to play a game?    ");
+    println!("  ---     ---     ---     ");
+    println!("");
+    println!("");
+    println!("  How about TicTacToe?");
+}
+
+fn main() {
+    title();
+
+}
